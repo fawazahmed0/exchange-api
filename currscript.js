f28f7867a3db7b475be08cc94687e5ee5210c33d
Fawaz Ahmed <fawazahmed0@hotmail.com>
2025-11-16 02:01:23 +0530

    Fix https://github.com/fawazahmed0/exchange-api/issues/135
    This bug was introduced due to https://github.com/fawazahmed0/exchange-api/issues/133

--- currscript.js
+++ currscript.js
@@ -20,6 +20,8 @@ let topCryptoCurrency = ["BTC","ETH","ADA","BNB","USDT","XRP","SOL","DOT","DOGE"
 
 let currencyCodesToRemove = ["CLF"]
 
+let isocodes = ["AFN","EUR","ALL","DZD","USD","AOA","XCD","XAD","ARS","AMD","AWG","AUD","AZN","BSD","BHD","BDT","BBD","BYN","BZD","XOF","BMD","INR","BTN","BOB","BOV","BAM","BWP","NOK","BRL","BND","BGN","BIF","CVE","KHR","XAF","CAD","KYD","CLP","CLF","CNY","COP","COU","KMF","CDF","NZD","CRC","CUP","XCG","CZK","DKK","DJF","DOP","EGP","SVC","ERN","SZL","ETB","FKP","FJD","XPF","GMD","GEL","GHS","GIP","GTQ","GBP","GNF","GYD","HTG","HNL","HKD","HUF","ISK","IDR","XDR","IRR","IQD","ILS","JMD","JPY","JOD","KZT","KES","KPW","KRW","KWD","KGS","LAK","LBP","LSL","ZAR","LRD","LYD","CHF","MOP","MKD","MGA","MWK","MYR","MVR","MRU","MUR","XUA","MXN","MXV","MDL","MNT","MAD","MZN","MMK","NAD","NPR","NIO","NGN","OMR","PKR","PAB","PGK","PYG","PEN","PHP","PLN","QAR","RON","RUB","RWF","SHP","WST","STN","SAR","RSD","SCR","SLE","SGD","XSU","SBD","SOS","SSP","LKR","SDG","SRD","SEK","CHE","CHW","SYP","TWD","TJS","TZS","THB","TOP","TTD","TND","TRY","TMT","UGX","UAH","AED","USN","UYU","UYI","UYW","UZS","VUV","VES","VED","VND","YER","ZMW","ZWG","XBA","XBB","XBC","XBD","XTS","XXX","XAU","XPD","XPT","XAG"]
+
 let currLink, currLink2, currLink3, currLink3Key, cryptoLink
 if (process.env.CI) {
   currLink = process.env.currlink
@@ -105,6 +107,7 @@ currDataObj3 = 'eur' in currDataObj3 ? toLowerCaseKeysBaseCurr(currDataObj3, 1/c
 let cryptoDataObj = await getCryptoData()
 // we also need to convert base usd to eur
 cryptoDataObj = toLowerCaseKeysBaseCurr(cryptoDataObj,currDataObj['usd'])
+isocodes.forEach(e=>delete cryptoDataObj?.[e.toLowerCase()])
   let CurrJSON = { ...currDataObj, ...currDataObj3, ...cryptoDataObj, eur: 1 }
   currencyCodesToRemove.forEach(e=>delete CurrJSON?.[e.toLowerCase()])
   // return sorted object
